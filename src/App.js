19c22cd24ecee1bc830a8ca4a14b9c54ba981f21
Tobias Augustyniak <tobias.augustyniak@t-online.de>
2023-04-27 17:17:28 +0200

    delete function completed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import "./App.css";
-import { useState } from "react";
 import { uid } from "uid";
 import Form from "./components/Form/Form";
 import useLocalStorageState from "use-local-storage-state";
 
 export default App;
 
-function List({ listItems }) {
-  return (
-    <ul>
-      {listItems.map((item) => {
-        return <li key={item.key}>{item.name}</li>;
-      })}
-    </ul>
-  );
-}
-
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-
+  function handleDeleteActivity(id) {
+    setActivities(activities.filter((activity) => activity.key !== id));
+  }
   function handleAddActivity(activitiesObject) {
     const id = uid();
     const activity = { key: id, ...activitiesObject };
     setActivities([activity, ...activities]);
     console.log(activities);
   }
-
+  function List({ listItems, onDeleteActivity }) {
+    return (
+      <ul>
+        {listItems.map((item) => {
+          return (
+            <li key={item.key}>
+              {item.name}
+              <button type="button" onClick={() => onDeleteActivity(item.key)}>
+                x
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
   return (
     <div className="App">
       <header className="App-header"></header>
       <h1>Weather App</h1>
-      <List listItems={activities} />
+      <List listItems={activities} onDeleteActivity={handleDeleteActivity} />
       <Form onAddActivity={handleAddActivity} />
     </div>
   );

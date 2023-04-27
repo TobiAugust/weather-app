edf91a444e8849c66b19f7cf779398c14d1019c0
Tobias Augustyniak <tobias.augustyniak@t-online.de>
2023-04-27 12:10:22 +0200

    Structure build, function checkbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+function formData(event) {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const data = Object.fromEntries(form);
+  const formElements = event.target.elements;
+  const dataClean = {
+    name: data.name,
+    isForGoodWeather: formElements.goodWeather.checked,
+  };
+  event.target.reset();
+  formElements.name.focus();
+}
+
+function Form({ onAddActivity }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={formData}>
+      <h2>Add New Activity:</h2>
+      <label>Name:</label>
+      <input name="name"></input>
+      <br />
+      <label>Good-Weather Activity:</label>
+      <input name="goodWeather" type="checkbox"></input>
+      <br />
+      <button className="button">Submit</button>
+    </form>
   );
 }
 
+function handleAddActivity(activities) {}
+
 export default App;
+
+function App() {
+  const [activities, setActivities] = useState();
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Weather App</h1>
+      <Form onAddActivity="" />
+    </div>
+  );
+}

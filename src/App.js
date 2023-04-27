ef96a1951b88244f150b40b9ad27c8c7f0cb5334
Tobias Augustyniak <tobias.augustyniak@t-online.de>
2023-04-27 13:02:24 +0200

    create components Folder seperate function Form and FormData

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import "./App.css";
 import { useState } from "react";
-
-function formData(event) {
-  event.preventDefault();
-  const form = new FormData(event.target);
-  const data = Object.fromEntries(form);
-  const formElements = event.target.elements;
-  const dataClean = {
-    name: data.name,
-    isForGoodWeather: formElements.goodWeather.checked,
-  };
-  event.target.reset();
-  formElements.name.focus();
-}
-
-function Form({ onAddActivity }) {
-  return (
-    <form onSubmit={formData}>
-      <h2>Add New Activity:</h2>
-      <label>Name:</label>
-      <input name="name"></input>
-      <br />
-      <label>Good-Weather Activity:</label>
-      <input name="goodWeather" type="checkbox"></input>
-      <br />
-      <button className="button">Submit</button>
-    </form>
-  );
-}
-
-function handleAddActivity(activities) {}
+import { uid } from "uid";
+import Form from "./components/Form/Form";
 
 export default App;
 
 function App() {
   const [activities, setActivities] = useState();
+
+  function handleAddActivity(activitiesObject) {
+    const uid = uid();
+    const activity = { key: uid, ...activities };
+    setActivities(activity, ...activities);
+    console.log(activities);
+  }
   return (
     <div className="App">
       <header className="App-header"></header>
       <h1>Weather App</h1>
-      <Form onAddActivity="" />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,28 @@
+function formData(event) {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const data = Object.fromEntries(form);
+  const formElements = event.target.elements;
+  const dataClean = {
+    name: data.name,
+    isForGoodWeather: formElements.goodWeather.checked,
+  };
+  event.target.reset();
+  formElements.name.focus();
+  return dataClean;
+}
+
+export default function Form({ onAddActivity }) {
+  return (
+    <form onSubmit={formData}>
+      <h2>Add New Activity:</h2>
+      <label>Name:</label>
+      <input name="name"></input>
+      <br />
+      <label>Good-Weather Activity:</label>
+      <input name="goodWeather" type="checkbox"></input>
+      <br />
+      <button className="button">Submit</button>
+    </form>
+  );
+}
